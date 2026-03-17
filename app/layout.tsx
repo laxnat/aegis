38b13429558af087c48c7c97be5cd48b3ff9eeac
Axl Tan <axllltan@gmail.com>
2026-03-16 20:01:48 -0700

    Set bg-secondary on <html> to prevent black overscroll flash

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-secondary">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${barlowCondensed.variable} ${bebasNeue.variable} antialiased`}
       >
